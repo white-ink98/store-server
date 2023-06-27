31d6f53df7dfb517d859e02464ea0c4818c5b817
white-ink98 <skorohod.gleb@gmail.com>
2023-06-27 19:50:39 +0300

    category products

--- products/views.py
+++ products/views.py
@@ -12,11 +12,17 @@ def index(request):
     return render(request, 'products/index.html', context)
 
 
-def products(request):
+def products(request, category_id=None):
+    if category_id:
+        category = ProductCategory.objects.get(id=category_id)
+        products = Product.objects.filter(category=category)
+    else:
+        products = Product.objects.all()
+
     context = {
         'title': 'Store - Каталог',
-        'products': Product.objects.all(),
         'categories': ProductCategory.objects.all(),
+        'products': products,
     }
     return render(request, 'products/products.html', context)
 
