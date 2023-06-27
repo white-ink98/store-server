44082ec9a8d6ecc91c63f9392b5bd02b059da0f4
white-ink98 <skorohod.gleb@gmail.com>
2023-06-27 18:32:03 +0300

    basket views/models

--- products/models.py
+++ products/models.py
@@ -23,11 +23,32 @@ class Product(models.Model):
         return f'Товар: {self.name} | Категорія: {self.category.name}'
 
 
+class BasketQuerySet(models.QuerySet):
+    def total_sum(self):
+        return sum(basket.sum() for basket in self)
+
+    def total_quantity(self):
+        return sum(basket.quantity for basket in self)
+
+
 class Basket(models.Model):
     user = models.ForeignKey(to=User, on_delete=models.CASCADE)
     product = models.ForeignKey(to=Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=0)
     created_timestamp = models.DateTimeField(auto_now_add=True)
 
+    objects = BasketQuerySet.as_manager()
+
     def __str__(self):
-        return f'Корзина для {self.user.email} | Продукт: {self.product.name}'
+        return f'Корзина для {self.user.username} | Продукт: {self.product.name}'
+
+    def sum(self):
+        return self.product.price * self.quantity
+
+#    def total_sum(self):
+#        baskets = Basket.objects.filter(user=self.user)
+#        return sum(basket.sum() for basket in baskets)
+#
+#    def total_quantity(self):
+#        baskets = Basket.objects.filter(user=self.user)
+#        return sum(basket.quantity for basket in baskets)
--- users/views.py
+++ users/views.py
@@ -5,6 +5,7 @@ from django.urls import reverse
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 
 from users.models import User
+from products.models import Basket
 
 
 def login(request):
@@ -45,7 +46,26 @@ def profile(request):
             return HttpResponseRedirect(reverse('users:profile'))
     else:
         form = UserProfileForm(instance=request.user)
-    context = {'title': 'Store - Профіль', 'form': form}
+
+    # baskets = Basket.objects.filter(user=request.user)
+    # total_sum = sum(basket.sum() for basket in baskets)
+    # total_quantity = sum(basket.quantity for basket in baskets)
+    #   total_sum = 0
+    #   total_quantity = 0
+    #   for basket in baskets:
+    #       total_sum += basket.sum()
+    #       total_quantity += basket.quantity
+    # total_sum = total_sum + basket.sum()
+    # total_quantity = total_quantity + basket.quantity
+
+    context = {
+        'title': 'Store - Профіль',
+        'form': form,
+        'baskets': Basket.objects.filter(user=request.user),
+        # 'total_sum': sum(basket.sum() for basket in baskets),
+        # 'total_quantity': sum(basket.quantity for basket in baskets),
+
+    }
     return render(request, 'users/profile.html', context)
 
 
